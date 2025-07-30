ae519f4fe99ce84657562b177602993aca35223c
Miquel <miquelroca08@gmail.com>
2025-07-30 13:12:51 +0200

    v0.7 Added Kernel Interrupts IDT and GDT, also Interrupt handling

--- build_scripts/config.mk
+++ build_scripts/config.mk
@@ -17,10 +17,11 @@ export TARGET_LD = $(TARGET)-gcc
 export TARGET_LINKFLAGS =
 export TARGET_LIBS =
 
+export SOURCE_DIR = $(abspath .)
 export BUILD_DIR = $(abspath build)
 
-BINUTILS_VERSION = 2.45
+BINUTILS_VERSION = 2.37
 BINUTILS_URL = https://ftp.gnu.org/gnu/binutils/binutils-$(BINUTILS_VERSION).tar.xz
 
-GCC_VERSION = 15.1.0
+GCC_VERSION = 11.2.0
 GCC_URL = https://ftp.gnu.org/gnu/gcc/gcc-$(GCC_VERSION)/gcc-$(GCC_VERSION).tar.xz
