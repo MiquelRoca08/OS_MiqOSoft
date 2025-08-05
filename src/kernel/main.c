9cf4bfeb4ca2cc324e17560c04583dffd0651cc7
Miquel <miquelroca08@gmail.com>
2025-08-05 11:10:34 +0200

    v0.15 Add memory detection and boot parameter handling in bootloader
    - Introduced memory detection functionality in memdetect.c and memdetect.h.
    - Updated main.c to utilize boot parameters for kernel execution.
    - Enhanced SConscripts to include project directory for library paths.
    - Added requirements.txt for dependency management.
    - Refactored install_deps.sh to simplify package installation.

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -4,6 +4,7 @@
 #include <hal/hal.h>
 #include <arch/i686/irq.h>
 #include <debug.h>
+#include <boot/bootparams.h>
 
 extern void _init();
 
@@ -14,19 +15,29 @@ void timer(Registers* regs)
     printf(".");
 }
 
-void start(uint16_t bootDrive)
+void start(BootParams* bootParams)
 {   
     // call global constructors
     _init();
 
     HAL_Initialize();
 
-    log_debug("Main", "This is a debug msg!");
+    log_debug("Main", "Boot device: %x", bootParams->BootDevice);
+    log_debug("Main", "Memory region count: %d", bootParams->Memory.RegionCount);
+    for (int i = 0; i < bootParams->Memory.RegionCount; i++) 
+    {
+        log_debug("Main", "MEM: start=0x%llx length=0x%llx type=%x", 
+            bootParams->Memory.Regions[i].Begin,
+            bootParams->Memory.Regions[i].Length,
+            bootParams->Memory.Regions[i].Type);
+    }
+
+
     log_info("Main", "This is an info msg!");
     log_warn("Main", "This is a warning msg!");
     log_err("Main", "This is an error msg!");
     log_crit("Main", "This is a critical msg!");
-    printf("OS MiqOSoft v0.14\n");
+    printf("OS MiqOSoft v0.15\n");
     printf("This operating system is under construction.\n");
     //i686_IRQ_RegisterHandler(0, timer);
 
